4863069cc34183c714cdf1ac5332c0dc658a14c4
Eric Zheng <eeeeeric@users.noreply.github.com>
2020-12-26 14:15:49 -0600

    Reformat files, slight optimization of Dockerfile

--- websalvador/entrypoint.sh
+++ websalvador/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 unitd --control unix:/var/run/control.unit.sock --log /var/log/unit.log
 curl -X PUT \
-  --data-binary @config.json \
+  --data-binary @/rSalvador/config.json \
   --unix-socket /var/run/control.unit.sock \
   http://localhost/config/
 tail -f /var/log/unit.log
